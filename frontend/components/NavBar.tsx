ec714091c6de639b90e1ed0abaa7c54a57cc0ca0
alxstankov <s23621614@onlineedu.tu-varna.bg>
2026-05-04 15:43:51 +0300

    Init

---
+++ frontend/components/NavBar.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image"
+import { RxHamburgerMenu } from "react-icons/rx"
+
+const NavBar = () => {
+    return (
+        <div className="w-full py-[6.5px] px-15.25 bg-background border-s-white border-2 z-50 fixed top-0 right-0 left-0">
+            <div className="flex flex-row gap-x-4 max-h-12.5">
+                <RxHamburgerMenu className="w-[50px] h-[50px]" />
+                <Image
+                    src='/MovieTuHub_new.png'
+                    height={50}
+                    width={162}
+                    loading="eager"
+                    alt="MovieTuHubs"
+                    className="object-cover object-center scale-150"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
